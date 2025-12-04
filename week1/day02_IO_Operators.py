6835e488a8c2c1d35c9e49f171165636ea7618a8
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-12-04 23:19:04 +0530

    Add Day2

---
+++ week1/day02_IO_Operators.py
@@ -0,0 +1,69 @@
+# #Input and Output
+#
+# #Simple Print
+# print("Hello Programming keeda")
+#
+# #print number
+# print(10)
+# print(56.22)
+#
+#
+# #printing multiple values
+# name = "Saurabh"
+# age = 28
+# print("Name: ",name, "Age: ", age)
+#
+# #using formated  string(f-String)
+# print (f"My Name is {name} and I am {age} years old." )
+#
+# #Input String
+# name = input("Enter your name: ")
+# print(f"My Name is {name}")
+# age = input("Enter your age: ")
+# print(f"My Age is {age}")
+# height = float(input("Enter your height: "))
+# print (f"My Height is {height}")
+
+
+#Operators in Python
+
+#Arithmetic Operators
+a = 10
+b = 3
+print(a + b)  # 13
+print(a - b)  # 7
+print(a * b)  # 30
+print(a / b)  # 3.3333
+print(a // b) # 3
+print(a % b)  # 1
+print(a ** b) # 1000
+
+#Comparison Operators
+x = 5
+y = 3
+print(x == y)  # False
+print(x != y)  # True
+print(x > y)   # True
+print(x < y)   # False
+print(x >= 5)  # True
+print(y <= 2)  # False
+
+#Logical Operators
+a = True
+b = False
+print(a and b)  # False
+print(a or b)   # True
+print(not a)    # False
+
+
+
+#Assignment Operators
+
+
+
+
+
+
+
+
+
