5f5310b4029475d239a6894eeec42c45c22dee3b
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-12-09 22:30:22 +0530

    Add Day3

--- week1/day02_IO_Operators.py
+++ week1/day02_IO_Operators.py
@@ -1,37 +1,10 @@
-# #Input and Output
-#
-# #Simple Print
-# print("Hello Programming keeda")
-#
-# #print number
-# print(10)
-# print(56.22)
-#
-#
-# #printing multiple values
-# name = "Saurabh"
-# age = 28
-# print("Name: ",name, "Age: ", age)
-#
-# #using formated  string(f-String)
-# print (f"My Name is {name} and I am {age} years old." )
-#
-# #Input String
-# name = input("Enter your name: ")
-# print(f"My Name is {name}")
-# age = input("Enter your age: ")
-# print(f"My Age is {age}")
-# height = float(input("Enter your height: "))
-# print (f"My Height is {height}")
-
-
 #Operators in Python
 
 #Arithmetic Operators
 a = 10
 b = 3
-print(a + b)  # 13
-print(a - b)  # 7
+print(f"The Sum Of {a} and {b} is { a + b}")  # 13
+print(f"The difference of {a} and {b} is {a- b}")  # 7
 print(a * b)  # 30
 print(a / b)  # 3.3333
 print(a // b) # 3
