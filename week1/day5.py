3988a6779c28c9f2f4f0f387de54094c0492628e
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-12-14 18:48:41 +0530

    Day 6 — Python for switch statement

--- week1/day5.py
+++
@@ -1,7 +0,0 @@
-#conditional Statement
-
-age = int(input("Enter your age: "))
-if(age>=18):
-    print("Person is eligible to vote.")
-else:
-    print("Person is ineligible to vote.")
---
+++ week1/day5_if_else.py
@@ -0,0 +1,26 @@
+# conditional Statement
+age = int(input("Enter your age: "))
+if (age >= 18 and age <= 45):
+    print("Person is eligible to vote twice.")
+elif (age > 45):
+    print("Person is eligible to vote ")
+else:
+    print("Person is ineligible to vote.")
+print("\n")
+print("----Menu----")
+print("1. Green")
+print("2. Red")
+print("3. Yellow")
+print("4. White")
+choice = int(input("Enter your choice number:"))
+if choice == 1:
+    print("Your Color is Green. ")
+elif choice == 2:
+    print("Your Color is Red. ")
+elif choice == 3:
+    print("Your Color is Yellow. ")
+elif choice == 4:
+    print("Your Color is White. ")
+else:
+    print("Invalid Choice.")
+
